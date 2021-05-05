6af35d0299bce25111d975a3c33c90becb5dd887
aidinrakanov <altynaikanatbekova@gmail.com>
2021-05-06 01:03:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/devtest/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.devtest
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.example.devtest.R
+import com.example.devtest.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navView: BottomNavigationView
+    private lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val badge = navView.getOrCreateBadge(R.id.navigation_orders)
+        badge.isVisible = true
+        badge.backgroundColor = ContextCompat.getColor(this, R.color.green)
+        badge.number = 2
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        setupNavigation()
+        navView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_search,
+                R.id.navigation_orders,
+                R.id.navigation_profile
+            ))
+
+    }
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
+}
